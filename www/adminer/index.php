3dedbe23cdb43d82ad8b432bd44b1d17694ba368
Michal Fíbek <michal@fibek.cz>
2015-02-07 10:53:28 +0100

    adminer updated to adminer-custom by david grudl

--- www/adminer/index.php
+++ www/adminer/index.php
@@ -1,21 +1,45 @@
 <?php
 
-// if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) || !isset($_SERVER['REMOTE_ADDR']) ||
-// 	!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
-// {
-// 	header('HTTP/1.1 403 Forbidden');
-// 	echo 'Adminer is available only from localhost';
-// 	for ($i = 2e3; $i; $i--) echo substr(" \t\r\n", rand(0, 3), 1);
-// 	exit;
-// }
+if (empty($_GET['file'])) {
+	ob_start(function($s) {
+		return preg_replace_callback('#(<(link|script)\s[^>]*(href|src)=")(adminer\.css|static/.+)"#U', function($m) {
+			return $m[1] . '?file=' . urlencode($m[4]) . '"';
+		}, $s);
+	}, 4096);
 
+} elseif (preg_match('#^(default|adminer|static(/\w[\w.-]*)+)\.(\w+)\z#', $_GET['file'], $m)) {
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+		header('HTTP/1.1 304 Not Modified');
+		exit;
+	}
 
-$root = __DIR__ . '/../../vendor/dg/adminer-custom';
+	header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime('1 month')) . ' GMT');
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
-if (!is_file($root . '/index.php')) {
-	echo "Install Adminer using `composer install`\n";
-	exit(1);
+	$types = array('css' => 'text/css', 'js' => 'text/javascript', 'gif' => 'image/gif', 'png' => 'image/png');
+	if (isset($types[$m[3]])) {
+		header('Content-Type: ' . $types[$m[3]]);
+	}
+	@readfile(__DIR__ . '/' . $_GET['file']);
+	exit;
 }
 
 
-require $root . '/index.php';
+function adminer_object() {
+	include_once dirname(__FILE__) . '/plugins/plugin.php';
+
+	foreach (glob(dirname(__FILE__) . '/plugins/*.php') as $filename) {
+		include_once $filename;
+	}
+
+	$plugins = array(
+		new AdminerDisableJush,
+		new AdminerAutocomplete,
+		new AdminerSaveMenuPos,
+		new AdminerRemoteColor,
+	);
+
+	return new AdminerPlugin($plugins);
+}
+
+include dirname(__FILE__) . '/adminer.php';
