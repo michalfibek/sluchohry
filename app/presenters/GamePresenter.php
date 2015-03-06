0548f65690ed2963d4d234fdd021fbd65112cf6d
Michal Fíbek <michal@fibek.cz>
2015-03-06 15:05:50 +0100

    melodic cubes basic game system

--- app/presenters/GamePresenter.php
+++ app/presenters/GamePresenter.php
@@ -9,6 +9,27 @@ use Nette,
  */
 class GamePresenter extends BasePresenter
 {
+	/** @var Nette\Database\Context */
+	private $database;
+	private $songCur;
+	private $splitDuration;
+	protected $splitCount = 8;
+	protected $shuffledOrder;
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+
+	public function actionMelodicCubes()
+	{
+		$songCount = $this->database->table('song')->count("*");
+		$randSongId = rand(1,$songCount);
+		$this->songCur = $this->database->table('song')->get($randSongId);
+		$this->splitDuration = $this->songCur->duration/$this->splitCount;
+		$this->shuffledOrder = range(1,$this->splitCount);
+		shuffle($this->shuffledOrder);
+	}
 
 	public function renderDefault()
 	{
@@ -18,5 +39,13 @@ class GamePresenter extends BasePresenter
     {
 
     }
+
+	public function renderMelodicCubes()
+	{
+		$this->template->song = $this->songCur;
+		$this->template->splitCount = $this->splitCount;
+		$this->template->splitDuration = $this->splitDuration;
+		$this->template->shuffledOrder = $this->shuffledOrder;
+	}
 
 }
