01c5f5646c10ba083a610fc11d7bf98c9f55f807
Michal Fíbek <michal@fibek.cz>
2015-03-10 14:54:47 +0100

    added "class" parent function definition

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,3 +1,19 @@
-$(function(){
-
-});
+/**
+ * Base class definition.
+ * @param definition
+ * @returns {*}
+ */
+var $class = function(definition) {
+    var constructor = definition.constructor;
+    var parent = definition.Extends;
+    if (parent) {
+        var F = function() { };
+        constructor._superClass = F.prototype = parent.prototype;
+        constructor.prototype = new F();
+    }
+    for (var key in definition) {
+        constructor.prototype[key] = definition[key];
+    }
+    constructor.prototype.constructor = constructor;
+    return constructor;
+};
