e9fdc231378c64f857ffa0eb0412435476130c57
Michal Fíbek <michal@fibek.cz>
2015-03-10 21:35:39 +0100

    updated melodic cubes admin module

--- app/presenters/AdminPresenter.php
+++ app/presenters/AdminPresenter.php
@@ -3,7 +3,8 @@
 namespace App\Presenters;
 
 use Nette,
-	App\Model;
+	App\Model,
+	Nette\Application\UI\Form;
 
 
 /**
@@ -11,6 +12,24 @@ use Nette,
  */
 class AdminPresenter extends BasePresenter
 {
+	protected function createComponentSongInfoForm()
+	{
+		$form = new Form;
+		$form->addText('artist', 'Artist:')
+			->setRequired();
+		$form->addText('title', 'Title:');
+		$form->addSubmit('send', 'Save');
+
+		$form->onSuccess[] = array($this, 'songInfoFormSubmitted'); // a přidat událost po odeslání
+
+		return $form;
+	}
+
+	public function songInfoFormSubmitted($form, $values)
+	{
+		$this->flashMessage("Příspěvek byl úspěšně publikován.", 'success');
+		$this->redirect('Homepage:');
+	}
 
 	public function actionSetRole($userId, $roleName)
 	{
@@ -24,43 +43,43 @@ class AdminPresenter extends BasePresenter
 
 	}
 
-
 	/**
 	 * uses Fine Uploader to handle uploaded music file
 	 * TODO - vyclenit praci s ukladanim souboru do samostatneho modelu
      */
 	public function handleUploadFile() {
-	$uploadDirName = __DIR__ . '/../../uploads/';
-	$targetDirName = $_SERVER['DOCUMENT_ROOT'] . '/assets/sounds/songs/';
-	$uploader = new \UploadHandler();
-	$uploader->allowedExtensions = array("mp3", "wav", "ogg");
-	try {
-		// TODO osefovat kontrolu duplicity souboru v databazi, jinak pokracovat
-		$result = $uploader->handleUpload($uploadDirName);
-		$uploadFilePath = $uploadDirName . $result['uuid'] . '/' . $uploader->getUploadName();
+		$uploadDirName = __DIR__ . '/../../uploads/';
+		$targetDirName = $_SERVER['DOCUMENT_ROOT'] . '/assets/sounds/songs/';
+		$uploader = new \UploadHandler();
+		$uploader->allowedExtensions = array("mp3", "wav", "ogg");
+		try {
+			// TODO osefovat kontrolu duplicity souboru v databazi, jinak pokracovat
+			$result = $uploader->handleUpload($uploadDirName);
+			$uploadFilePath = $uploadDirName . $result['uuid'] . '/' . $uploader->getUploadName();
 
-		$getID3 = new \getID3;
-		$fileInfo = $getID3->analyze($uploadFilePath);
-		\getid3_lib::CopyTagsToComments($fileInfo); // merges all detected tags and copies them into single 'comments' array (or 'comments_html')
-		$duration = round($fileInfo['playtime_seconds']*1000);
-		$artist = implode(' & ', $fileInfo['comments']['artist']); // merges artist names if more of them are present
-		$title = $fileInfo['comments']['title'][0];
+			$getID3 = new \getID3;
+			$fileInfo = $getID3->analyze($uploadFilePath);
+			\getid3_lib::CopyTagsToComments($fileInfo); // merges all detected tags and copies them into single 'comments' array (or 'comments_html')
+			$duration = round($fileInfo['playtime_seconds']*1000);
+			$artist = implode(' & ', $fileInfo['comments']['artist']); // merges artist names if more of them are present
+			$title = $fileInfo['comments']['title'][0];
 
-		$result['ext'] = $uploader->getFileExtension();
-		$result['artist'] = $artist;
-		$result['title'] = $title;
-		$result['duration'] = $duration . ' ms';
+			$result['ext'] = $uploader->getFileExtension();
+			$result['artist'] = $artist;
+			$result['title'] = $title;
+			$result['duration'] = $duration/1000 . ' s';
+			$result['filename'] = $uploader->getUploadName();
 
-		$targetFilePath = $targetDirName . $result['uuid'] . '.' . $fileInfo['fileformat'];
+			$targetFilePath = $targetDirName . $result['uuid'] . '.' . $fileInfo['fileformat'];
 
-		Nette\Utils\FileSystem::copy($uploadFilePath, $targetFilePath);
-		Nette\Utils\FileSystem::delete(__DIR__ . '/../../uploads/' . $result['uuid']);
-	} catch (\Exception $exc) {
-		$this->sendResponse(new Nette\Application\Responses\JsonResponse(array(
-			'error' => $exc->getMessage(),
-		)));
+			Nette\Utils\FileSystem::copy($uploadFilePath, $targetFilePath);
+			Nette\Utils\FileSystem::delete(__DIR__ . '/../../uploads/' . $result['uuid']);
+		} catch (\Exception $exc) {
+			$this->sendResponse(new Nette\Application\Responses\JsonResponse(array(
+				'error' => $exc->getMessage(),
+			)));
+		}
+		$this->sendResponse(new Nette\Application\Responses\JsonResponse($result));
 	}
-	$this->sendResponse(new Nette\Application\Responses\JsonResponse($result));
-}
 
 }
