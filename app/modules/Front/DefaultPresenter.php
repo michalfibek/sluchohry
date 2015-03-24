1f9e899e0ee9206417574c4d9f3c20f4c2e680f1
Michal Fíbek <michal@fibek.cz>
2015-03-24 12:46:15 +0100

    login form moved to default presenter

--- app/modules/Front/DefaultPresenter.php
+++ app/modules/Front/DefaultPresenter.php
@@ -10,6 +10,58 @@ use Tracy\Debugger;
  */
 class DefaultPresenter extends \App\Module\Base\Presenters\BasePresenter
 {
+	/** @persistent */
+	public $backlink;
+
+	/**
+	 * Sign-in form factory.
+	 * @return Nette\Application\UI\Form
+	 */
+	protected function createComponentLoginForm()
+	{
+		$form = new Nette\Application\UI\Form;
+		$form->setTranslator(
+			$this->translator->domain('front.auth.loginForm')
+		);
+		$form->addText('username', 'username')
+			->setRequired('requiredUsername');
+
+		$form->addPassword('password', 'password')
+			->setRequired('requiredPassword');
+
+		$form->addCheckbox('remember', 'remember');
+
+		$form->addSubmit('send', 'loginButton');
+
+		$form->onSuccess[] = array($this, 'processLoginForm');
+		return $form;
+	}
+
+	public function processLoginForm($form, $values)
+	{
+		if ($values->remember) {
+			$this->getUser()->setExpiration('14 days', FALSE);
+		} else {
+			$this->getUser()->setExpiration('20 minutes', TRUE);
+		}
+
+		try {
+			$this->getUser()->login($values->username, $values->password);
+			$this->restoreRequest($this->backlink);
+			$this->redirect(':Front:Default:default');
+
+		} catch (Nette\Security\AuthenticationException $e) {
+			$form->addError($e->getMessage());
+		}
+	}
+
+	public function actionLogout()
+	{
+		$this->getUser()->logout();
+		$this->flashMessage('front.auth.flash.logout');
+		$this->redirect('Default:');
+	}
+
 	public function renderDefault()
 	{
 
