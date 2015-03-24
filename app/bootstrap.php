09cf91a09ca3ec2750706d6273e83fd05c329a78
Michal Fíbek <michal@fibek.cz>
2015-03-24 20:52:04 +0100

    cookie based debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode(['192.168.1.10', '188.75.144.92']);
+$configurator->setDebugMode(['secretCookie147@192.168.1.10', 'secretCookie147@188.75.144.92']);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
@@ -18,5 +18,8 @@ $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
 
 $container = $configurator->createContainer();
+
+//setcookie('nette-debug', 'secretCookie147', strtotime('1 years'), '/', '', '', TRUE); // DANGER! beware to run on production
+//setcookie('nette-debug', 'secretCookie147', 1, '/', '', '', TRUE); // FOR COOKIE UNSET
 
 return $container;
