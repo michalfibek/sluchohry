543823a0cd4fd8fe9644e4eed4e57dd6e36c596e
Michal Fíbek <michal@fibek.cz>
2015-04-02 13:18:04 +0200

    listener constructor cleaned

--- app/model/UserListener.php
+++ app/model/UserListener.php
@@ -8,16 +8,10 @@ use Nette,
 class UserListener extends Nette\Object implements \Kdyby\Events\Subscriber
 {
     private $logger;
-    private $user;
-    private $httpRequest;
-    private $userAgent;
 
-    function __construct(EventLogger $logger, Nette\Security\User $user, Nette\Http\Request $httpRequest)
+    function __construct(EventLogger $logger)
     {
         $this->logger = $logger;
-        $this->user = $user;
-        $this->httpRequest = $httpRequest;
-        $this->userAgent = $this->httpRequest->getHeader('User-Agent');
     }
 
     public function getSubscribedEvents()
@@ -27,7 +21,7 @@ class UserListener extends Nette\Object implements \Kdyby\Events\Subscriber
             'App\Module\Base\Presenters\BaseGamePresenter::onGameEnd' => 'onGameEnd',
             'App\Module\Base\Presenters\BaseGamePresenter::onGameForceEnd' => 'onGameForceEnd',
             'Nette\Application\Application::onError' => 'onError',
-            'App\Module\Base\Presenters\BasePresenter::onStartup' => 'onStartup',
+//            'App\Module\Base\Presenters\BasePresenter::onStartup' => 'onStartup',
             'Nette\Security\User::onLoggedIn' => 'onLoggedIn',
             'Nette\Security\User::onLoggedOut' => 'onLoggedOut'
         );
@@ -36,7 +30,6 @@ class UserListener extends Nette\Object implements \Kdyby\Events\Subscriber
     public function onGameStart($result)
     {
         $this->logger->saveGameStart($result);
-//        Debugger::barDump('Game start: '. $gameName, $result);
     }
 
     public function onGameEnd($result)
