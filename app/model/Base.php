f153e7fffe66028be22ca2a36ed2d06b02565ff4
Michal Fíbek <michal@fibek.cz>
2015-04-03 12:39:22 +0200

    Base model added

---
+++ app/model/Base.php
@@ -0,0 +1,214 @@
+<?php
+namespace App\Model;
+
+use Nette;
+
+abstract class Base extends Nette\Object
+{
+    const TABLE_PREFIX = '';
+
+    protected $db;
+
+    /** @var string */
+    protected $tableName;
+
+
+    public function __construct(\Nette\Database\Context $connection)
+    {
+        $this->db = $connection;
+        $this->tableName = $this->tableNameByClass(get_class($this));
+    }
+
+
+    /**
+     * Gets table name by class
+     * @param string
+     * @return string
+     * @result: Pages => pages, ArticleTag => article_tag
+     */
+    private function tableNameByClass($className)
+    {
+        $tableName = explode("\\", $className);
+        $tableName = strtr(array_pop($tableName), array('Insecure' => ''));
+        $tableName = lcfirst($tableName);
+
+        $replace = array('insecure' => ''); // A => _a
+        foreach (range("A", "Z") as $letter) {
+            $replace[$letter] = "_" . strtolower($letter);
+        }
+
+        return self::TABLE_PREFIX . strtr($tableName, $replace);
+    }
+
+    /**
+     * Returns table row by primary id
+     *
+     * @param int id - primary id
+     * @return bool|row - false or row itself
+     */
+    public function getById($id)
+    {
+        return $this->db->table($this->tableName)->get($id);
+    }
+
+    /**
+     * Inserts data into table
+     *
+     * @param array $data - assarray
+     * @return boolean|ActiveRow - false pri neuspechu, nebo vlozeny radek
+     */
+    public function insert($data)
+    {
+        return $this->db->table($this->tableName)->insert($data);
+    }
+
+    /**
+     * Uprava radku v DB dle id primarniho klice
+     *
+     * @param int $id - id primarniho klice radku s daty
+     * @param array $data - pole s daty ve formatu column => value
+     * @return int|FALSE pocet ovlivnenych radku nebo FALSE pri chybe
+     */
+    public function updateById($id, $data)
+    {
+        if (is_array($id)) {
+            return $this->db->table($this->tableName)->where('id', $id)->update($data);
+        } else {
+            return $this->db->table($this->tableName)->wherePrimary($id)->update($data);
+        }
+    }
+
+    /**
+     * Deletes row by primary ID
+     * @param $id
+     * @return int
+     */
+    public function deleteById($id)
+    {
+        return $this->db->table($this->tableName)->wherePrimary($id)->delete();
+    }
+
+
+    /**
+     * Deletes everything from table
+     * @return int
+     */
+    public function deleteAll()
+    {
+        return $this->db->table($this->tableName)->delete();
+    }
+
+    /**
+     * Fetch data from table with limit
+     *
+     * @param int $limit
+     * @param int $offset
+     * @return \Nette\Database\Table\Selection
+     */
+    public function get($limit = 30, $offset = 0)
+    {
+        return $this->getAll()->limit($limit, $offset);
+    }
+
+    /**
+     * Fetch everything from table
+     *
+     * @return \Nette\Database\Table\Selection
+     */
+    public function getAll()
+    {
+        return $this->db->table($this->tableName);
+    }
+
+    /**
+     * Fetches data into array grouped by $column
+     *
+     * @param $column
+     * @return array
+     */
+    public function getGroupAsArray($column = "name")
+    {
+        $array = array();
+        $rows = $this->getAll()->group($column);
+        foreach ($rows as $row)
+            $array[$row->$column] = $row->$column;
+        return $array;
+    }
+
+    /**
+     * Vypis dat z tabulky do pola podla id
+     *
+     * @param $column
+     * @return array
+     */
+    public function getGroupAsArrayWithId($column = "name")
+    {
+        $array = array();
+        $rows = $this->getAll()->select("id, " . $column);
+        foreach ($rows as $row)
+            $array[$row->id] = $row->$column;
+        return $array;
+    }
+
+    /**
+     * Fetches everything as array
+     *
+     * @param $column
+     * @return array
+     */
+    public function getAsArray($column = "name")
+    {
+        $array = array();
+        $rows = $this->getAll()->select("id, ".$column);
+        foreach ($rows as $row)
+            $array[$row->id] = $row->$column;
+        return $array;
+    }
+
+    /**
+     * @param $firstColumn
+     * @param $secondColumn
+     * @return array
+     */
+    public function getAllPairs($firstColumn, $secondColumn)
+    {
+        $array = array();
+        $rows = $this->getAll();
+        foreach ($rows as $row) {
+            $array[$row->$firstColumn] = $row->$secondColumn;
+        }
+        return $array;
+    }
+
+    /**
+     * Fetches count of rows in table
+     *
+     * @return bool|int - count of rows
+     */
+    public function getCount()
+    {
+        return $this->db->table($this->tableName)->count('*');
+    }
+
+    /**
+     * Select particular columns from table
+     *
+     * @param $columns
+     * @return $this|Nette\Database\Table\Selection
+     */
+    public function select($columns)
+    {
+        return $this->db->table($this->tableName)->select($columns);
+    }
+
+    /**
+     * Applies standard query.
+     *
+     * @param $query
+     * @return Nette\Database\ResultSet
+     */
+    public function query($query)
+    {
+        return $this->db->query($query);
+    }
+}
