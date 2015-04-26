110955fbaf98be35cb4675eed81def64e6a2e4b5
Michal Fíbek <michal@fibek.cz>
2015-04-26 23:50:04 +0200

    batch login creator prepared (via csv file)

---
+++ bin/batch-create-user.php
@@ -0,0 +1,39 @@
+<?php
+
+$container = require __DIR__ . '/../app/bootstrap.php';
+$manager = $container->getByType('App\Model\User');
+
+
+foreach ($users as $u) {
+	list($username, $realname, $email, $password) = explode(';', $u);
+
+	$groupId = 5; /// GROUP DEF
+
+	$insertData = array(
+		'username' => $username,
+		'password' => $password,
+		'email' => $email,
+		'realname' => $realname,
+		'avatar_id' => 1,
+		'group_id' => $groupId,
+	);
+
+	try {
+		$manager->insert($insertData);
+		echo "User $username was added.\n";
+
+	} catch (App\Model\DuplicateNameException $e) {
+		echo "Error: duplicate name.\n";
+		exit(1);
+	}
+}
+
+
+try {
+	$manager->add($user, $password);
+	echo "User $user was added.\n";
+
+} catch (App\Model\DuplicateNameException $e) {
+	echo "Error: duplicate name.\n";
+	exit(1);
+}
