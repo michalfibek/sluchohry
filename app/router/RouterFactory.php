08f3ac54bd0920e4499cb824f2b86b1a34d8dffc
Michal Fíbek <michal@fibek.cz>
2015-05-17 15:14:13 +0200

    URL fix AGAIN

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -19,7 +19,7 @@ class RouterFactory extends Nette\Object
 
 	public function __construct(Nette\Http\Request $httpRequest)
 	{
-		if ($httpRequest->getUrl()->getHost() === ('sluchohry.cz' || 'dp.sluchohry.cz'))
+		if (in_array($httpRequest->getUrl()->getHost(), array('sluchohry.cz', 'dp.sluchohry.cz')))
 			$this->useHttps = true;
 		else
 			$this->useHttps = false;
